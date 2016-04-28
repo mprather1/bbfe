3452cc0d959945381809143ed6595b6de08350f9
Michael Prather <mprather1@gmail.com>
2016-04-28 18:16:35 -0500

    api working correctly

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,19 @@
 class UsersController < ApplicationController
 
+#  skip_before_filter :verify_authenticity
+
   def index
+    @users = User.all
+    respond_to do |format|
+      format.json { render json: @users }
+    end
   end
-  
+
+  def show
+    @user = User.find(params[:id])
+    respond_to do |format|
+      format.json { render json: @user }
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-  resources :users
+  get 'users' => 'users#index'
+  get 'user/:id/' => 'users#show'
 end
