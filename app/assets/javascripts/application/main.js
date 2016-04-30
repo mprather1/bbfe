da0eaad6b3e4409e3da6577f2a7c5d08d88292fd
Michael Prather <mprather1@gmail.com>
2016-04-29 22:54:58 -0500

    cleaned up

--- app/assets/javascripts/application/main.js
+++ app/assets/javascripts/application/main.js
@@ -1,12 +1,4 @@
- // $(function() {
- //   var userGroupView = new allUsersView();
- // });
-
-// var userGroupView = new allUsersView();
-// $('#allUsers').html(userGroupView.render().el);
-// $('#allUsers').html(userGroupView.el)
 $(function() {
   var userGroupView = new allUsersView();
   $('#allUsers').html(userGroupView.el)
-
 });
--- app/assets/javascripts/application/views/userCollectionView.js
+++ app/assets/javascripts/application/views/userCollectionView.js
@@ -24,38 +24,5 @@ allUsersView = Backbone.View.extend({
     });
     this.$el.append(user.render().el);
   }
-  // className: 'usersList',
-  //
-  // render: function() {
-  //   for (var i = 0; i < this.collection.length; i++) {
-  //     this.renderItem(this.collection.models[i]);
-  //   };
-  //   $(this.container).find(this.className).remove();
-  //   this.$el.appendTo(this.options.container);
-  //   return this;
-  // },
-  //
-  // renderItem: function(model) {
-  //   var item = new singleUserView({
-  //     "model": model
-  //   });
-  //   item.render().$el.appendTo(this.$el);
-  // }
-
-  // render: function() {
-  //   this.collection.each(this.addUser, this);
-  //   return this;
-  // },
-  //
-  // addUser: function(user) {
-  //   var userView = singleUserView ({ model: user });
-  //   this.$el.append(userView.render().el);
-  // }
-
-});
-
-$(function() {
-  var userGroupView = new allUsersView();
-  $('#allUsers').html(userGroupView.el)
 
 });
